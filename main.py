bf999adbe0da229cad44f67ce1d03796ffdf0ff9
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2025-03-08 10:52:28 +0700

    adjust log

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import time
 from utils.email_reader import baca_email
+from datetime import datetime, timedelta
 
 if __name__ == "__main__":
+    start_time = datetime.now()  # Waktu mulai program
+
     try:
         while True:
+            # Hitung durasi runtime
+            elapsed_time = datetime.now() - start_time
+            total_hours = elapsed_time.total_seconds() // 3600
+            total_days = elapsed_time.total_seconds() // 86400
+
+            print("=" * 50)
+            print(f"[🕒] Service berjalan selama: {int(total_days)} hari, {int(total_hours) % 24} jam")
             print("[🔍] Cek Email...")
+            
             baca_email()
+            
             print("[⏳] Tunggu 1 jam...\n")
-            time.sleep(3600)
+            time.sleep(3600)  # Tunggu 1 jam
     except KeyboardInterrupt:
-        print("Program dihentikan secara manual.")
+        print("\n[❌] Program dihentikan secara manual.")
